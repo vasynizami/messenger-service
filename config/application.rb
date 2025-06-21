867fe639a0aeb1700ff49463d10541926de1835a
Vasilia Nizami <vasilianizami@gmail.com>
2025-06-21 19:34:50 -0400

    skip master key during init

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,8 @@ module MessengerService
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.assets.initialize_on_precompile = false
-    config.assets.enabled = false
+    config.require_master_key = false
+    # config.assets.enabled = false
+    # config.assets.initialize_on_precompile = false
   end
 end
