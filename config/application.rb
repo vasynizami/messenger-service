6e487bcdffddd444184119911e0a0b791291b6ff
Vasilia Nizami <vasilianizami@gmail.com>
2025-06-21 19:04:25 -0400

    build fix

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module MessengerService
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.initialize_on_precompile = false
   end
 end
