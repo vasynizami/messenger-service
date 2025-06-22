32498e8005ac0d262de0849593655c212fbe2c3e
Vasilia Nizami <vasilianizami@gmail.com>
2025-06-21 20:45:12 -0400

    remove cfgs with assets

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module MessengerService
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.assets.enabled = false
-    config.assets.initialize_on_precompile = false
+    # config.assets.enabled = false
+    # config.assets.initialize_on_precompile = false
     config.require_master_key = false
   end
 end
